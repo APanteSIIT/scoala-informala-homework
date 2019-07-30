cf93f4e33267a480fccba2856a61c9b29fd2e232
APanteSIIT <anamaria_pante@yahoo.com>
2019-07-30 19:27:54 +0300

    tema punctul 2- HashMap updated Persoane class with multiple iteration methods added JavaDoc on Persoane class added extra JavaDoc comments

--- src/temaWeek7HashMap/Persoane.java
+++ src/temaWeek7HashMap/Persoane.java
@@ -15,7 +15,7 @@ package temaWeek7HashMap;
 
 import java.util.*;
 
-public class Persoane {
+public class Persoane  {
 	
 	private String name;
 	private int age;
@@ -46,20 +46,21 @@ public class Persoane {
 	
 	
 	public static void main(String[] args) {
+//		create student,somer angajat objects
 		Student student = new Student("Ionel", 25);
 		Somer somer = new Somer("Vasile", 55);
 		Angajat angajat = new Angajat("Maria", 35);
-		
+//		create adresses objects
 		Adresa adresa1 = new Adresa("Campuilui", 1, "Rasnov", "Ro");
 		Adresa adresa2 = new Adresa("Zorilor", 25, "Brasov", "Ro");
 		Adresa adresa3 = new Adresa("Victoriei", 55, "Bucuresti", "Ro");
 		Adresa adresa4 = new Adresa("Lincoln Avenue", 55, "Washington", "USA");
-		
+//		create hobby objects
 		Hobby cyclism = new Hobby("cyclism", 2, adresa1);
 		Hobby football = new Hobby("football", 1, adresa2);
 		Hobby handball = new Hobby("handball", 3, adresa3);
 		Hobby chess = new Hobby("chess", 3, adresa4);
-		
+//		created lists of hobbies
 		List<Hobby> hobbies1 = new ArrayList<>();
 		hobbies1.add(cyclism);
 		hobbies1.add(football);
@@ -78,27 +79,23 @@ public class Persoane {
 		map.put(angajat, hobbies2);
 		map.put(somer, hobbies3);
 		
-	
+//	  multiple method to iterate thru a map
 		// forEach(action) method to iterate map
 		map.forEach((k,v) -> System.out.println("Key = "
 				+ k + ", Value = " + v));
-//		// using for-each loop for iteration over Map.entrySet()
-		System.out.println(map.containsKey(student));
+		
+		// using for-each loop for iteration over Map.entrySet()
 		for (Map.Entry<Persoane, List<Hobby>> entry : map.entrySet()) {
-			System.out.println("Key=" + entry.getKey() + "value=" + entry.getValue());
+			System.out.println("Name= " + entry.getKey().getName() + " " + entry.getValue());
 		}
 		
 		// using values() for iteration over keys
 		for (List<Hobby> h : map.values())
 			System.out.println("value: " + hobbies1);
 		
-		
-		
+	
 		
 	}
-	
-	
-
 	
 	@Override
 	public boolean equals(Object o) {
@@ -122,4 +119,6 @@ public class Persoane {
 				"age=" +
 				'}';
 	}
+	
+	
 }
