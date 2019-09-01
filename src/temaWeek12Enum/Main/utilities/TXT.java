ee02bd6d8bd64081255e6cc3103d3b6fe536b68a
APanteSIIT <anamaria_pante@yahoo.com>
2019-09-01 11:30:50 +0300

    created class Person with 3variables :name, birthCate and gender added in class Person an enum class Gender updated Person class and added file1.txt and file2.txt to utilities folder created AppPerson class added a class for finding files based on extension from a specific directory added code for calculating the time of main thread running and added javaDoc added javadoc to TXT class

---
+++ src/temaWeek12Enum/Main/utilities/TXT.java
@@ -0,0 +1,52 @@
+package temaWeek12Enum.Main.utilities;
+
+import temaWeek12Enum.Main.Person;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TXT {
+//	method that reads from a file and returns a list of array strings
+	public static List<String[]> readFromFile(String fileName) {
+		
+		List<String[]> list = new ArrayList<>();
+		
+		String line;
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			while ((line = br.readLine())!=null) {
+				String[] dateRecords = line.split(",");
+				list.add(dateRecords);
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("FileNotFound exception found");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("IOException was caught!");
+			e.printStackTrace();
+		}
+		
+		
+		return list;
+	}
+//	method to write in a new file from a list of Persons
+	public static void writeToFile(List<Person> list) {
+		
+		try (BufferedWriter bw = new BufferedWriter(new PrintWriter("Female.txt"))) {
+			for (int i = 0; i < list.size(); i++) {
+				String birthDate = list.get(i).getBirthDate();
+				String name = list.get(i).getName();
+//				writing to a file with a specific format
+				bw.write("Happy Birthday " + list.get(i).getFirstName(name) + "(" +
+						list.get(i).getYearOfBirth(birthDate) + ")!");
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			System.out.println("IOException was found");
+			e.printStackTrace();
+		}
+	}
+	
+}
+	
+
