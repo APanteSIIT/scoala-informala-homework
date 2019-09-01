aa9cc1e2025472be9b0bb35381a3cc038e4bafab
APanteSIIT <anamaria_pante@yahoo.com>
2019-09-01 11:34:18 +0300

    created class Person with 3variables :name, birthCate and gender added in class Person an enum class Gender updated Person class and added file1.txt and file2.txt to utilities folder created AppPerson class added a class for finding files based on extension from a specific directory added code for calculating the time of main thread running and added javaDoc added javadoc to TXT class added javadoc and added methods for getting firstname and month&day of birth

--- src/temaWeek12Enum/Main/Person.java
+++ src/temaWeek12Enum/Main/Person.java
@@ -1,54 +1,68 @@
 package temaWeek12Enum.Main;
 
-import java.util.Date;
-
 public class Person {
-
-	private String name;
-	private String birthDate;
+	
+	private final String name;
+	private final String birthDate;
 	private String gender;
 	
-	public Person(String name,String birthDate,String gender){
-		this.name=name;
-		this.birthDate=birthDate;
-		this.gender=gender;
+	public Person(String name, String birthDate, String gender) {
+		this.name = name;
+		this.birthDate = birthDate;
+		//calling getGender from enum Gender class for further use of enum const.
+		this.gender = Gender.getGender(gender);
 	}
+//	enum class for gender
+	public enum Gender {
+		FEMALE, MALE;
+		
+		public static String getGender(String input) {
+			if (input.equals("0") || input.equals("male")) {
+				return MALE.toString();
+			} else if (input.equals("1") || input.equals("female")) {
+				return FEMALE.toString();
+			} else throw new IllegalArgumentException("Enter a valid gender");
+		}
+		
+	}
+	
 	
 	public String getName() {
 		return name;
 	}
 	
-	public void setName(String name) {
-		this.name = name;
-	}
 	
 	public String getBirthDate() {
 		return birthDate;
-	}
-	
-	public void setBirthDate(String birthDate) {
-		this.birthDate = birthDate;
 	}
 	
 	public String getGender() {
 		return gender;
 	}
 	
-	public void setGender(String gender) {
-		this.gender = gender;
+//	method to extract the month and day from the birthDate
+	public String getDayAndMonthOfBirth(){
+		int index=birthDate.indexOf("-");
+		return birthDate.substring(index+1);
 	}
 	
-	public enum Gender{
-		FEMALE,MALE;
-		
-		public static Gender getGender(String input){
-			if(input.equals("0")||input.equalsIgnoreCase("male")){
-				return MALE;
-			}
-			else if(input.equals("1")||input.equalsIgnoreCase("female")){
-				return FEMALE;
-			}
-			else throw new IllegalArgumentException("Enter a valid gender");
-		}
+//	method to extract the year of birth
+	public String getYearOfBirth(String birthDate) {
+		return birthDate.substring(0, 4);
+	}
+	
+//	method to extract firstName from name
+	public String getFirstName(String name) {
+		int index = getName().lastIndexOf(" ");
+		return getName().substring(0, index);
+	}
+	
+	@Override
+	public String toString() {
+		return "Person{" +
+				"name='" + name + '\'' +
+				", birthDate='" + birthDate + '\'' +
+				", gender='" + gender.toLowerCase() + '\'' +
+				'}' + "\n";
 	}
 }
