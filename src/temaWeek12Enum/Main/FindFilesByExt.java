a80f94a5622bc1849af9cb29309a092c0313aa07
APanteSIIT <anamaria_pante@yahoo.com>
2019-09-01 11:35:33 +0300

    created class Person with 3variables :name, birthCate and gender added in class Person an enum class Gender updated Person class and added file1.txt and file2.txt to utilities folder created AppPerson class added a class for finding files based on extension from a specific directory added code for calculating the time of main thread running and added javaDoc added javadoc to TXT class added javadoc and added methods for getting firstname and month&day of birth reformat code for FindFilesByExt class

--- src/temaWeek12Enum/Main/FindFilesByExt.java
+++ src/temaWeek12Enum/Main/FindFilesByExt.java
@@ -4,30 +4,28 @@ import java.io.File;
 import java.io.FilenameFilter;
 
 
-
 public class FindFilesByExt implements FilenameFilter {
 	
 	
-		private static String extension;
+	private static String extension;
+	
+	public FindFilesByExt(String extension) {
 		
-		public FindFilesByExt(String extension) {
-			
-			this.extension=extension;
-		}
-		
-		@Override
-		public boolean accept(File loc,String name) {
-			if(name.lastIndexOf(".")>0){
-				int lastIndex=name.lastIndexOf(".");
-				String str=name.substring(lastIndex);
-				if(extension.equalsIgnoreCase(str)){
-					return true;
-				}
-			}
-			return false;
-		}
-		
-		
-		
+		this.extension = extension;
 	}
+	
+	@Override
+	public boolean accept(File loc, String name) {
+		if (name.lastIndexOf(".") > 0) {
+			int lastIndex = name.lastIndexOf(".");
+			String str = name.substring(lastIndex);
+			if (extension.equalsIgnoreCase(str)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	
+}
 
